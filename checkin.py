8d024870ac2163b2afba706add9001a4b232931a
小尭 <82511711+XiaoPigYao@users.noreply.github.com>
2021-10-16 23:14:04 +0800

    上传Qmsg酱

--- checkin.py
+++ checkin.py
@@ -20,6 +20,26 @@ def md5(str):
 def protect(text):
     return {"params":encrypt('TA3YiYCfY2dDJQgg',encrypt('0CoJUm6Qyw8W8jud',text)),"encSecKey":"84ca47bca10bad09a6b04c5c927ef077d9b9f1e37098aa3eac6ea70eb59df0aa28b691b7e75e4f1f9831754919ea784c8f74fbfadf2898b0be17849fd656060162857830e241aba44991601f137624094c114ea8d17bce815b0cd4e5b8e2fbaba978c6d1d14dc3d1faf852bdd28818031ccdaaa13a6018e1024e2aae98844210"}
 
+def QmsChan(qmsgkey:str,desp:str):
+    '''
+    推送Qms酱
+    '''
+    if qmsgkey == '' :
+        print("[注意] 未提供qmsgkey，不进行推送！")
+    else:
+        qqtalk = 'https://qmsg.zendee.cn:443/send/'+qmsgkey
+        qmsg_data = {
+            "msg":desp,
+            "qq":"260374918"
+        }
+        response = requests.post(qqtalk,data=qmsg_data)
+        json_qmsgdata=response.json()
+        # print(json_qmsgdata)
+        if json_qmsgdata['success'] == True:
+            print(f"[{now}]：Qmsg酱推送成功！")
+        else:
+            print(f"[{now}]Qms酱推送失败：{json_qmsgdata['reason']}。code:{json_qmsgdata['code']}")
+
 
 s=requests.Session()
 header={}
